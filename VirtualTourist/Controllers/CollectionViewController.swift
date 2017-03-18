bd1a7d2dc25b6d87c9b8a8f8ab578d91e1068741
Milad Nozari <mnvoh90@gmail.com>
2017-03-18 12:52:41 +0300

    feat: Created the view controllers

---
+++ VirtualTourist/Controllers/CollectionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewController.swift
+//  VirtualTourist
+//
+//  Created by Milad Nozari on 3/18/17.
+//  Copyright © 2017 Nozary. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+}
